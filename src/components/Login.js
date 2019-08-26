c78f75ca842fc9cc16b73a7ff5fd4070672d3234
gumar <gumar@focusolusi.onmicrosoft.com>
2019-08-26 18:35:44 +0700

    Login Tampilan Ok

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,53 +1,47 @@
 import React, { Component } from 'react';
+import moduleName from '../css/css.css';
 
 class Login extends Component {
-    renderList() {
-       
-            return (
-                <div className="d-flex justify-content-center h-100">
-	            	<div className="card">
-	            		<div className="card-header">
-	            			<h3>Sign In</h3>
-	            			<div className="d-flex justify-content-end social_icon">
-	            				<span><i className="fab fa-facebook-square"></i></span>
-	            				<span><i className="fab fa-google-plus-square"></i></span>
-	            				<span><i className="fab fa-twitter-square"></i></span>
-	            			</div>
-	            		</div>
-	            		<div className="card-body">
-	            			<form>
-	            				<div className="input-group form-group">
-	            					<div className="input-group-prepend">
-	            						<span className="input-group-text"><i className="fas fa-user"></i></span>
-	            					</div>
-	            					<input type="text" className="form-control" placeholder="username"/>
-	            				</div>
-	            				<div className="input-group form-group">
-	            					<div className="input-group-prepend">
-	            						<span className="input-group-text"><i className="fas fa-key"></i></span>
-	            					</div>
-	            					<input type="password" className="form-control" placeholder="password"/>
-	            				</div>
-	            				
-	            				<div className="form-group">
-	            					<input type="submit" value="Login" className="btn float-right login_btn"/>
-	            				</div>
-	            			</form>
-	            		</div>
-	            		<div className="card-footer">
-	            			
-	            			<div className="d-flex justify-content-center">
-	            				Forgot your password?
-	            			</div>
-	            		</div>
-	            	</div>
-	            </div>
-                );
-    }
+	renderLogin() {
+		return (
+			<div className="column twelve wide" >
+					<h2 className="ui image header">
+						<div className="content">
+							Log-in to your account
+      					</div>
+					</h2>
+					<form action="https://s.codepen.io/voltron2112/debug/PqrEPM?" method="get" class="ui large form">
+						<div className="ui stacked secondary  segment">
+							<div className="field">
+								<div className="ui left icon input">
+									<i className="user icon"></i>
+									<input type="text" name="email" placeholder="E-mail address"/>
+          						</div>
+							</div>
+								<div className="field">
+									<div className="ui left icon input">
+										<i className="lock icon"></i>
+										<input type="password" name="password" placeholder="Password"/>
+         				 </div>
+									</div>
+									<div className="ui fluid large teal submit button">Login</div>
+								</div>
 
+								<div className="ui error message"></div>
+
+   					</form>
+
+							<div className="ui message">
+								New to us? <a href="https://s.codepen.io/voltron2112/debug/PqrEPM?">Register</a>
+							</div>
+			</div>
+				
+					);
+		}
+	
     render() {
-        return <div className="container">{this.renderList()}</div>
-    };
-}
-
+        return <div className="ui middle aligned center aligned grid">{this.renderLogin()}</div>
+					};
+				}
+				
 export default Login;
