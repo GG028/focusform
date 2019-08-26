1ccf5e86a869aa87fea050cb43ef15c769bc874c
gumar <gumar@focusolusi.onmicrosoft.com>
2019-08-26 17:39:55 +0700

    penampilan awal

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App';
 
-const App = () =>{
-    return <div>Hi There !</div>
-};
+
 
 ReactDOM.render(<App/>, document.querySelector('#root'));
