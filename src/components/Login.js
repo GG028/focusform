31ee70950ae04c39ead92fa0d5b67ced1d61cc71
gumar <gumar@focusolusi.onmicrosoft.com>
2019-08-27 10:52:33 +0700

    Ok

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,47 +1,52 @@
 import React, { Component } from 'react';
-import moduleName from '../css/css.css';
+import css from '../css/css.css';
+
+const style = {
+	height: '80px',
+	'verticalAlign': 'middle',
+	'textAlign': 'center',
+	'lineHeight': '20px'
+  }
 
 class Login extends Component {
+	
 	renderLogin() {
 		return (
 			<div className="column twelve wide" >
-					<h2 className="ui image header">
-						<div className="content">
-							Log-in to your account
+				<i className="user circle icon" size='large'></i>
+				<div className="content">
+					Log-in to your account
       					</div>
-					</h2>
-					<form action="https://s.codepen.io/voltron2112/debug/PqrEPM?" method="get" class="ui large form">
-						<div className="ui stacked secondary  segment">
-							<div className="field">
-								<div className="ui left icon input">
-									<i className="user icon"></i>
-									<input type="text" name="email" placeholder="E-mail address"/>
-          						</div>
+				<form action="https://s.codepen.io/voltron2112/debug/PqrEPM?" method="get" className="ui large form">
+					<div className="ui stacked secondary  segment">
+						<div className="field">
+							<div className="ui left icon input">
+								<i className="user icon"></i>
+								<input type="text" name="email" placeholder="E-mail address" />
 							</div>
-								<div className="field">
-									<div className="ui left icon input">
-										<i className="lock icon"></i>
-										<input type="password" name="password" placeholder="Password"/>
-         				 </div>
-									</div>
-									<div className="ui fluid large teal submit button">Login</div>
-								</div>
-
-								<div className="ui error message"></div>
-
-   					</form>
-
-							<div className="ui message">
-								New to us? <a href="https://s.codepen.io/voltron2112/debug/PqrEPM?">Register</a>
+						</div>
+						<div className="field">
+							<div className="ui left icon input">
+								<i className="lock icon"></i>
+								<input type="password" name="password" placeholder="Password" />
 							</div>
+						</div>
+						<div className="ui fluid large teal submit button">Login</div>
+					</div>
+
+					<div className="ui error message"></div>
+
+				</form>
+
+			
 			</div>
-				
-					);
-		}
-	
-    render() {
-        return <div className="ui middle aligned center aligned grid">{this.renderLogin()}</div>
-					};
-				}
-				
+
+		);
+	}
+
+	render() {
+		return <css><div style={style} className="ui middle aligned center aligned grid">{this.renderLogin()}</div></css>
+	};
+}
+
 export default Login;
